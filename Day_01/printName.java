04688236d59919a5b42fd00c7f8202d5f60697b2
ANISH KANRAR <147317580+Anish-Kanrar@users.noreply.github.com>
2024-07-31 23:31:49 +0530

    Add files via upload

---
+++ Day_01/printName.java
@@ -0,0 +1,9 @@
+package CollegeJAVA.Day_01;
+
+public class printName {
+    public static void main(String args[]){
+        System.out.println("Name:Anish Kanrar");
+        System.out.println("Roll:CSE/22/033");
+        System.out.println("ygpa:7.57");
+    }
+}
---
+++ Day_01/sum.java
@@ -0,0 +1,10 @@
+package CollegeJAVA.Day_01;
+
+public class sum {
+    public static void main(String args[]){
+        int a=7;
+        int b=5;
+        int sum=a+b;
+        System.out.println("Sum is: "+sum);
+    }
+}
