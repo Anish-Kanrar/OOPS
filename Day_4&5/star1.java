ee6ffbedd950d0ac7c7d33f9ea86d3471b312882
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-09-11 22:26:25 +0530

    Update star1.java

--- Day_4&5/star1.java
+++ Day_4&5/star1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class star1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number:");
         int n=sc.nextInt();
